6bc96daa4e52f41529b68783c3c48607d0552d3d
Maxim <u04@asv.me>
2024-03-27 16:53:33 +0300

    feat(sdr): add select first payload in sdr
    asana: https://app.asana.com/0/1203851531040615/1206919176847518/f

--- src/Asv.Drones.Gui.Plugin.Sdr/Tools/Controls/SdrPayloadBrowserViewModel.cs
+++ src/Asv.Drones.Gui.Plugin.Sdr/Tools/Controls/SdrPayloadBrowserViewModel.cs
@@ -14,14 +14,13 @@ using ReactiveUI.Fody.Helpers;
 namespace Asv.Drones.Gui.Plugin.Sdr;
 
 [Export]
-
-public class SdrPayloadBrowserViewModel:ViewModelBase
+public class SdrPayloadBrowserViewModel : ViewModelBase
 {
     private readonly ILocalizationService _loc;
     private readonly ILogService _log;
     private readonly ReadOnlyObservableCollection<SdrDeviceViewModel> _devices;
-    
-    public SdrPayloadBrowserViewModel():base(SdrWellKnownUri.SdrDeviceBrowser)
+
+    public SdrPayloadBrowserViewModel() : base(SdrWellKnownUri.SdrDeviceBrowser)
     {
         if (Design.IsDesignMode)
         {
@@ -34,138 +33,126 @@ public class SdrPayloadBrowserViewModel:ViewModelBase
                 }));
         }
     }
-    
-    public SdrPayloadBrowserViewModel(IMavlinkDevicesService mavlink, ILocalizationService loc, ILogService log):this()
+
+    public SdrPayloadBrowserViewModel(IMavlinkDevicesService mavlink, ILocalizationService loc, ILogService log) :
+        this()
     {
         _loc = loc;
         _log = log;
         mavlink
             .Payloads
-            .Transform(_ => new SdrDeviceViewModel(_,loc,log))
+            .Transform(_ => new SdrDeviceViewModel(_, loc, log))
             .Bind(out _devices)
             .DisposeMany()
             .Subscribe()
             .DisposeItWith(Disposable);
-        this.WhenValueChanged(_=>SelectedDevice)
-            .Where(_=>_ != null)
-            .Subscribe(_=>_.DownloadRecords.Execute().Subscribe())
-            .DisposeItWith(Disposable);
+        SelectedDevice = _devices.FirstOrDefault();
         
-        this.WhenAnyValue(_ => _.SelectedDevice)
-            .Subscribe(_ =>
-            {
-                IsAnySelected = _ != null;
-            })
+        this.WhenValueChanged(_ => SelectedDevice)
+            .Where(_ => _ != null)
+            .Subscribe(_ => _.DownloadRecords.Execute().Subscribe())
             .DisposeItWith(Disposable);
-       
+
+        this.WhenAnyValue(_ => _.SelectedDevice)
+            .Subscribe(_ => { IsAnySelected = _ != null; })
+            .DisposeItWith(Disposable);
     }
 
-    public ReactiveCommand<Unit,Unit> Refresh { get; set; }
+    public ReactiveCommand<Unit, Unit> Refresh { get; set; }
 
     public ReadOnlyObservableCollection<SdrDeviceViewModel> Devices => _devices;
-    
-    [Reactive]
-    public SdrDeviceViewModel? SelectedDevice { get; set; }
 
-    [Reactive]
-    public bool IsAnySelected { get; set; } 
-    
+    [Reactive] public SdrDeviceViewModel? SelectedDevice { get; set; }
+
+    [Reactive] public bool IsAnySelected { get; set; }
+
     public void TrySelect(Guid recordId)
     {
         SelectedDevice?.TrySelect(recordId);
     }
 }
 
-public class SdrDeviceViewModel:ViewModelBase
+public class SdrDeviceViewModel : ViewModelBase
 {
     private readonly ILocalizationService _loc;
     private readonly ILogService _log;
     private readonly ReadOnlyObservableCollection<SdrPayloadRecordViewModel> _items;
 
-    public SdrDeviceViewModel(ushort id):base(SdrWellKnownUri.SdrShellPageSdrStoreDeviceIndex.FormatWith(id))
+    public SdrDeviceViewModel(ushort id) : base(SdrWellKnownUri.SdrShellPageSdrStoreDeviceIndex.FormatWith(id))
     {
         Name = $"Payload {id}";
         if (Design.IsDesignMode)
         {
-            _items = new ReadOnlyObservableCollection<SdrPayloadRecordViewModel>(new ObservableCollection<SdrPayloadRecordViewModel>(new List<SdrPayloadRecordViewModel>
-            {
-                new(Guid.NewGuid())
+            _items = new ReadOnlyObservableCollection<SdrPayloadRecordViewModel>(
+                new ObservableCollection<SdrPayloadRecordViewModel>(new List<SdrPayloadRecordViewModel>
                 {
-                    Name = "Record1",
-                    CreatedDateTime = DateTime.Now.AddMinutes(-60),
-                },
-                new(Guid.NewGuid())
-                {
-                    Name = "Record2",
-                    CreatedDateTime = DateTime.Now.AddMinutes(-50),
-                },
-                new(Guid.NewGuid())
-                {
-                    Name = "Record3",
-                    CreatedDateTime = DateTime.Now.AddMinutes(-40),
-                }
-            }));
+                    new(Guid.NewGuid())
+                    {
+                        Name = "Record1",
+                        CreatedDateTime = DateTime.Now.AddMinutes(-60),
+                    },
+                    new(Guid.NewGuid())
+                    {
+                        Name = "Record2",
+                        CreatedDateTime = DateTime.Now.AddMinutes(-50),
+                    },
+                    new(Guid.NewGuid())
+                    {
+                        Name = "Record3",
+                        CreatedDateTime = DateTime.Now.AddMinutes(-40),
+                    }
+                }));
         }
     }
-    public SdrDeviceViewModel(ISdrClientDevice device, ILocalizationService loc,ILogService log):this(device.Heartbeat.FullId)
+
+    public SdrDeviceViewModel(ISdrClientDevice device, ILocalizationService loc, ILogService log) : this(
+        device.Heartbeat.FullId)
     {
         _loc = loc;
         _log = log;
         Client = device;
-        
+
         device.Sdr.Records
-            .Transform(_=>new SdrPayloadRecordViewModel(device.Heartbeat.FullId, _, _log, _loc, device.Sdr))
-            .SortBy(_ => IsSortByName ? _.Name : _.CreatedDateTime, 
+            .Transform(_ => new SdrPayloadRecordViewModel(device.Heartbeat.FullId, _, _log, _loc, device.Sdr))
+            .SortBy(_ => IsSortByName ? _.Name : _.CreatedDateTime,
                 IsSortByName ? SortDirection.Ascending : SortDirection.Descending)
             .Bind(out _items)
             .DisposeMany()
             .Subscribe()
             .DisposeItWith(Disposable);
-        
         this.WhenValueChanged(_ => SelectedRecord)
-            .Where(_=>_ != null)
-            .Subscribe(_=>_.DownloadTags.Execute().Subscribe())
+            .Where(_ => _ != null)
+            .Subscribe(_ => _.DownloadTags.Execute().Subscribe())
             .DisposeItWith(Disposable);
-        
         DownloadRecords = ReactiveCommand.CreateFromTask(cancel =>
                 device.Sdr.DownloadRecordList(new Progress<double>(_ => DownloadRecordsProgress = _), cancel))
             .DisposeItWith(Disposable);
-        
+
         DownloadRecords.ThrownExceptions.Subscribe(_ => _log.Error("Record", "Error to download records", _))
             .DisposeItWith(Disposable);
-        
+
         this.WhenAnyValue(_ => _.SelectedRecord)
-            .Subscribe(_ =>
-            {
-                IsAnySelected = _ != null;
-            })
+            .Subscribe(_ => { IsAnySelected = _ != null; })
             .DisposeItWith(Disposable);
-        
+
         this.WhenAnyValue(_ => _.IsSortByName)
-            .Subscribe(_ =>
-            {
-                DownloadRecords.Execute().Subscribe();
-            })
+            .Subscribe(_ => { DownloadRecords.Execute().Subscribe(); })
             .DisposeItWith(Disposable);
     }
 
-    [Reactive] 
-    public bool IsSortByName { get; set; } = true;
-    
-    [Reactive]
-    public double DownloadRecordsProgress { get; set; }
-    public ReactiveCommand<Unit,bool> DownloadRecords { get; }
+    [Reactive] public bool IsSortByName { get; set; } = true;
+
+    [Reactive] public double DownloadRecordsProgress { get; set; }
+    public ReactiveCommand<Unit, bool> DownloadRecords { get; }
 
     public string Name { get; set; }
-    
+
     public ReadOnlyObservableCollection<SdrPayloadRecordViewModel> Items => _items;
-    
-    [Reactive]
-    public SdrPayloadRecordViewModel SelectedRecord { get; set; }
-    
-    [Reactive]
-    public bool IsAnySelected { get; set; }
-    
+
+    [Reactive] public SdrPayloadRecordViewModel SelectedRecord { get; set; }
+
+    [Reactive] public bool IsAnySelected { get; set; }
+
     public ISdrClientDevice Client { get; }
 
     public void TrySelect(Guid recordId)
@@ -175,6 +162,3 @@ public class SdrDeviceViewModel:ViewModelBase
         SelectedRecord = item;
     }
 }
-
-
-
